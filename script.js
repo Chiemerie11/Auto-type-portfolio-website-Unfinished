891cdce218353965b385b7e5cbba8c477b3429f9
Chiemerie Oguejiofor <96776816+Chiemerie11@users.noreply.github.com>
2022-04-29 00:43:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-input", {
+    strings: ["Chiemerie Oguejiofor", "a Web Developer", "a Blockchain enthusiast", "also a Finance expert"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
