f6580314858dcd267c70c06edf46a9cbf94f60cb
Chiemerie Oguejiofor <96776816+Chiemerie11@users.noreply.github.com>
2022-04-29 00:33:24 +0100

    Delete script2.js

--- script2.js
+++
@@ -1,6 +0,0 @@
-let typed = new Typed(".auto-input", {
-    strings: ["Chiemerie Oguejiofor", "a Web Developer", "a Blockchain enthusiast", "also a Finance expert"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-});
